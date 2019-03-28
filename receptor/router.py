aed69df6b4cfd251e324c7a07f73cc6fa093ab56
Joshua "jag" Ginsberg <jag@flowtheory.net>
2019-03-28 17:39:14 -0400

    Basic Djikstra routing mesh.

--- receptor/router.py
+++ receptor/router.py
@@ -1,3 +1,12 @@
+import logging
+logger = logging.getLogger(__name__)
+
+from collections import defaultdict
+import heapq
+import random
+
+from receptor import get_node_id
+
 async def forward(outer_envelope, next_hop):
     """
     Forward a message on to the next hop closer to its destination
@@ -10,10 +19,58 @@ def next_hop(recipient):
     given recipient. If the current node is the recipient, then return
     None.
     """
-    raise NotImplementedError()
+    return router.find_shortest_path(recipient)
 
 async def send(outer_envelope):
     """
     Send a new message with the given outer envelope.
     """
     raise NotImplementedError()
+
+class MeshRouter:
+    _nodes = set()
+    _edges = set()
+
+    def node_is_known(self, node_id):
+        return node_id in self._nodes
+    
+    def register_node(self, node_id):
+        self._nodes.add(node_id)
+    
+    def register_edge(self, left, right, cost):
+        self._edges.add(
+            (
+                left if left < right else right,
+                right if left < right else left,
+                cost
+            )
+        )
+
+    def find_shortest_path(self, to_node_id):
+        """Implementation of Dijkstra algorithm"""
+        cost_map = defaultdict(list)
+        for left, right, cost in self._edges:
+            cost_map[left].append((cost, right))
+            cost_map[right].append((cost, left))
+
+        heap, seen, mins = [(0, get_node_id(), [])], set(), {get_node_id(): 0}
+        while heap:
+            (cost, vertex, path) = heapq.heappop(heap)
+            if vertex not in seen:
+                seen.add(vertex)
+                path = [vertex] + path                
+                if vertex == to_node_id:
+                    logger.debug(f'Shortest path to {to_node_id} with cost {cost} is {path}')
+                    return path[-1]
+                cost_map_for_vertex = cost_map.get(vertex, ())
+                random.shuffle(cost_map_for_vertex)
+                for next_cost, next_vertex in cost_map.get(vertex, ()):
+                    if next_vertex in seen:
+                        continue
+                    min_so_far = mins.get(next_vertex, None)
+                    next_total_cost = cost + next_cost
+                    if min_so_far is None or next_total_cost < min_so_far:
+                        mins[next_vertex] = next_total_cost
+                        heapq.heappush(heap, (next_total_cost, next_vertex, path))
+    
+router = MeshRouter()
