85f59ce46f37f2e541230d59b72b4768b9a3309b
Jesse Jaggars <jjaggars@redhat.com>
2019-03-28 17:03:18 -0400

    adding connection handshake stuff

--- receptor/messages/directive.py
+++ receptor/messages/directive.py
@@ -1,20 +1,23 @@
 import logging
-logger = logging.getLogger(__name__)
 from ..exceptions import UnknownDirective
 from .. import router
 from . import envelope
+
+logger = logging.getLogger(__name__)
+
 
 class Directive:
     def __init__(self, type_, payload):
         self.type_ = type_
         self.payload = payload
 
+
 class Control:
     CONTROL_DIRECTIVES = ['ping']
 
     async def __call__(self, inner_env):
         _, action = inner_env.directive.split(':', 1)
-        if not action in self.CONTROL_DIRECTIVES:
+        if action not in self.CONTROL_DIRECTIVES:
             raise UnknownDirective(f'Unknown control directive: {action}')
         action_method = getattr(self, action)
         responses = action_method(inner_env)
@@ -28,5 +31,6 @@ class Control:
                 serial=serial
             )
             await router.send(enveloped_response)
+
 
 control = Control()
