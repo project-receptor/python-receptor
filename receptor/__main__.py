f486326732894a66278f289e798498361e4bb251
Jesse Jaggars <jjaggars@redhat.com>
2019-03-28 14:18:53 -0400

    making __main__ work

--- receptor/__main__.py
+++ receptor/__main__.py
@@ -6,16 +6,19 @@ from .events import mainloop
 logger = logging.getLogger(__name__)
 
 
-def main(sys_args=None):
+def main(args=None):
     parser = argparse.ArgumentParser("receptor")
     parser.add_argument("-c", "--config", default="./receptor.conf")
     parser.add_argument("--listen-address", default="0.0.0.0")
     parser.add_argument("--listen-port", default=8888)
     parser.add_argument("-p", "--peer", action='append')
-    args = parser.parse_args(sys_args)
+    args = parser.parse_args(args)
     config = SafeConfigParser()
     config.read([args.config])
     mainloop(config.get('server', 'listen', fallback=args.listen_address),
              config.get('server', 'port', fallback=args.listen_port),
              peers=args.peer if args.peer else [],
     )
+
+
+main()
