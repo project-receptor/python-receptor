d099b2b65eec02f5837e95340df0b083d94b8eb8
Jesse Jaggars <jjaggars@redhat.com>
2019-03-30 13:25:22 -0400

    logging startup only once
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- receptor/events.py
+++ receptor/events.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 PING_INTERVAL = 15
 
+
 def mainloop(config):
     loop = asyncio.get_event_loop()
     if not config.server.server_disable:
@@ -15,20 +16,18 @@ def mainloop(config):
             BasicProtocol,
             config.server.address, config.server.port)
         loop.create_task(listener)
-        logger.info('Serving on {}'.format("{}:{}".format(config.server.address,
-                                                          config.server.port)))
+        logger.info("Serving on %s:%s", config.server.address, config.server.port)
     for peer in config.peers:
         loop.create_task(create_peer(peer.split(":")[0], peer.split(":")[1]))
     ping_time = (((int(loop.time()) + 1) // PING_INTERVAL) + 1) * PING_INTERVAL
     loop.call_at(ping_time, send_pings_and_reschedule, loop, ping_time)
-    logger.info('Serving on {}'.format("{}:{}".format(config.server.address,
-                                                      config.server.port)))
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         pass
     finally:
         loop.stop()
+
 
 async def send_pings_and_reschedule(loop, ping_time):
     for node_id in router.get_nodes():
