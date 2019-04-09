a5fd10c115bd4ce58c6ac6db5bd65fac08b2c20b
Joshua "jag" Ginsberg <jag@flowtheory.net>
2019-04-09 16:19:43 -0400

    Move cmdline to subcommands

--- receptor/__main__.py
+++ receptor/__main__.py
@@ -1,8 +1,8 @@
 import argparse
 import logging
 import logging.config
-from .config import ReceptorConfig
-from .events import mainloop
+from .config import ReceptorConfig, DEFAULT_CONFIG
+from .node import mainloop
 from receptor import Receptor
 
 logger = logging.getLogger(__name__)
@@ -22,16 +22,45 @@ def map_args_to_config(args):
     return to_return
 
 
+def run_as_node(args):
+    config = ReceptorConfig(args.config, map_args_to_config(args))
+    receptor = Receptor(config)
+    logger.info("Running as Receptor node with ID: {}".format(receptor.node_id))
+    mainloop(receptor)
+
+
 def main(args=None):
     parser = argparse.ArgumentParser("receptor")
-    parser.add_argument("-c", "--config", default="./receptor.conf")
-    parser.add_argument("--listen-address")
-    parser.add_argument("--listen-port")
-    parser.add_argument("-p", "--peer", action='append')
-    parser.add_argument("--debug", action="store_true", default=False)
-    parser.add_argument("--node-id")
-    parser.add_argument("--server-disable", action="store_true", default=False)
-    parser.add_argument("--ping")
+    parser.add_argument(
+        "-c", "--config", default="./receptor.conf",
+        help='Path to configuration file')
+    parser.add_argument(
+        "--debug", action="store_true", default=False,
+        help='Emit debugging output')
+
+    subparsers = parser.add_subparsers(
+        title='subcommands')
+    
+    subparser_node = subparsers.add_parser(
+        'node',
+        help='Run a receptor node')
+    subparser_node.add_argument(
+        "--listen-address",
+        help=f'Set/override IP address to listen on. If not set here or in a config file, the default is {DEFAULT_CONFIG["server"]["address"]}')
+    subparser_node.add_argument(
+        "--listen-port",
+        help=f'Set/override TCP port to listen on. If not set here or in a config file, the default is {DEFAULT_CONFIG["server"]["port"]}')
+    subparser_node.add_argument(
+        "-p", "--peer", action='append',
+        help=f'Set/override peer nodes/controllers to connect to. Use multiple times for multiple peers.')
+    subparser_node.add_argument(
+        "--node-id",
+        help='Set/override node identifier. If unspecified here or in a config file, one will be automatically generated.')
+    subparser_node.add_argument(
+        "--server-disable", action="store_true", default=False,
+        help="Disable the server function and only connect to configured peers.")
+    subparser_node.set_defaults(func=run_as_node)
+
     args = parser.parse_args(args)
     
     logging.config.dictConfig(
@@ -58,10 +87,7 @@ def main(args=None):
             },
         }
     )
-    config = ReceptorConfig(args.config, map_args_to_config(args))
-    receptor = Receptor(config)
-    logger.info("Node Id: {}".format(receptor.node_id))
-    mainloop(receptor)
+    args.func(args)
 
 
 main()
