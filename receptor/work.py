0f597b281437eada903839753d92fa65ab718390
Matthew Jones <matburt@redhat.com>
2019-05-14 16:22:44 -0400

    Fix up entry-point importer
    import_module needs iter_modules, in this case we can look for entry_points and pull-assign them based on the declared name

--- receptor/work.py
+++ receptor/work.py
@@ -1,4 +1,4 @@
-import importlib
+import pkg_resources
 import logging
 
 from . import exceptions
@@ -10,14 +10,17 @@ class WorkManager:
     def __init__(self, receptor):
         self.receptor = receptor
 
+    def load_receptor_worker(self, name):
+        entry_points = [x for x in filter(lambda x: x.name == name,
+                                          pkg_resources.iter_entry_points("receptor.worker"))]
+        if not entry_points:
+            raise exceptions.UnknownDirective("Error loading directive handlers for {name}")
+        return entry_points[0].load()
+
     async def handle(self, inner_env):
         logger.info(f'Handling work for {inner_env.message_id} as {inner_env.directive}')
         namespace, action = inner_env.directive.split(':', 1)
-        try:
-            worker_module = importlib.import_module(f'receptor.worker.{namespace}')
-        except ImportError:
-            logger.exception(f'Error importing worker module for {namespace}')
-            raise exceptions.UnknownDirective(f'Error loading directive handlers for {namespace}')
+        worker_module = self.load_receptor_worker(namespace)
         try:
             action_method = getattr(worker_module, f'do_{action}')
         except AttributeError:
@@ -25,7 +28,7 @@ class WorkManager:
             raise exceptions.InvalidDirectiveAction(f'Invalid action {action} for {namespace}')
         responses = action_method(inner_env)
         serial = 0
-        async for response in responses:
+        for response in responses:
             serial += 1
             logger.debug(f'Response emitted for {inner_env.message_id}, serial {serial}')
             enveloped_response = envelope.InnerEnvelope.make_response(
