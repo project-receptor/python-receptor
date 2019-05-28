ef9d00c40f0e0c60985b00cd87b5fdcfdc63680b
Matthew Jones <matburt@redhat.com>
2019-05-28 11:16:55 -0400

    Fixing up action behavior and reverting response handling

--- receptor/work.py
+++ receptor/work.py
@@ -22,13 +22,13 @@ class WorkManager:
         namespace, action = inner_env.directive.split(':', 1)
         worker_module = self.load_receptor_worker(namespace)
         try:
-            action_method = getattr(worker_module, f'do_{action}')
+            action_method = getattr(worker_module, f'{action}')
         except AttributeError:
             logger.exception(f'Could not load action {action} from {namespace}')
             raise exceptions.InvalidDirectiveAction(f'Invalid action {action} for {namespace}')
         responses = action_method(inner_env)
         serial = 0
-        for response in responses:
+        async for response in responses:
             serial += 1
             logger.debug(f'Response emitted for {inner_env.message_id}, serial {serial}')
             enveloped_response = envelope.InnerEnvelope.make_response(
