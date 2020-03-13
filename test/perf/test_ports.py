18e1fac81b2940e958e37f37fd6f2df318d4205b
Jeremy Audet <jerebear@protonmail.com>
2020-03-13 13:45:53 -0400

    Add test for absent port number
    If no port number is specified, then receptor should listen on port
    8888.
    
    See: https://github.com/project-receptor/receptor/issues/138

--- test/perf/test_ports.py
+++ test/perf/test_ports.py
@@ -1,6 +1,7 @@
 """Test port binding logic."""
 from uuid import uuid4
 
+import psutil
 import pytest
 from receptor_affinity import utils
 from receptor_affinity.exceptions import NodeUnavailableError
@@ -20,3 +21,21 @@ def test_invalid_listen_scheme():
     node = Node(str(uuid4()), listen=f'harglebargle://127.0.0.1:{utils.random_port()}')
     with pytest.raises(NodeUnavailableError):
         node.start()
+
+
+def test_no_port_given():
+    """Start a node, and don't specify a port on which to listen.
+
+    Assert that it listens on port 8888. Older versions of receptor would listen on a random port.
+    See: `receptor #138`_.
+
+    .. _receptor #138: https://github.com/project-receptor/receptor/issues/138
+    """
+    node = Node(str(uuid4()), listen=f'receptor://127.0.0.1')
+    node.start()
+    try:
+        conns = psutil.Process(node.pid).connections()
+        assert len(conns) == 1
+        assert conns[0].laddr.port == 8888
+    finally:
+        node.stop()
