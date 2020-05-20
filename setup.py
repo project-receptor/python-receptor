f3329fef368effc1250147f51747a6f075e6d011
Graham Mainwaring <graham@mhn.org>
2020-05-20 14:38:47 -0400

    Update version number for 0.6.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name="receptor",
-    version="0.6.2",
+    version="0.6.3",
     author='Red Hat',
     url="https://github.com/project-receptor/receptor",
     license='Apache',
